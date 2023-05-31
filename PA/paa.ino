dcfac41383969ef78c63118208f50b46ad962964
Dody Nadapdap <82352439+dodynadapdap@users.noreply.github.com>
2023-05-31 19:21:28 +0700

    Add files via upload

---
+++ PA/paa.ino
@@ -0,0 +1,30 @@
+String voice;
+int
+relay1 = 11, //Connect RELAY 1 To Pin #2
+relay2 = 10 //Connect RELAY 2 To Pin #3
+;
+//-----------------------------------------------------------------------//
+void setup() {
+Serial.begin(9600);
+pinMode(relay1, OUTPUT);
+pinMode(relay2, OUTPUT);
+
+digitalWrite(relay1, HIGH);
+digitalWrite(relay2, HIGH);
+}
+//-----------------------------------------------------------------------//
+void loop() {
+while (Serial.available()){  //
+delay(10); //
+char c = Serial.read(); //
+if (c == '#') {break;} //
+voice += c; //
+}
+if (voice.length() > 0) {
+
+ 
+//----------kontrol setiap relay dengan perintah---------------//
+if(voice == "open") {digitalWrite(relay1, LOW);}
+else if(voice == "close") {digitalWrite(relay1, HIGH      );}
+//-----------------------------------------------------------------------//
+voice="";}} //
